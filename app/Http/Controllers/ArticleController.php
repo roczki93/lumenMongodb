06009ab7799790566d8f985185b9152924399574
lukasz.rodak <lukasz.rodak@softiq.pl>
2017-06-22 21:56:24 +0200

    add new data article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -18,11 +18,33 @@ class ArticleController extends Controller
 	return response()->json($articles);
      //
     }
-
+    public function getArticle($id){
+  
+        $article  = Article::find($id);
+  
+        return response()->json($article);
+    }article
     public function createArticle(Request $request){
 	$article = Article::create($request->all());
 	
 	return response()->json($article);
+    }
+
+    public function deleteArticle($id){
+        $article  = Article::find($id);
+        $article->delete();
+ 
+        return response()->json('deleted');
+    }
+    public function updateArticle(Request $request,$id){
+        $article  = Article::find($id);
+        $article->title = $request->input('title');
+        $article->content = $request->input('content');
+        $article->coment = $request->input('coment');
+        $article->short = $request->input('short');
+        $article->save();
+  
+        return response()->json($article);
     }
     //
 }
