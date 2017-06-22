a4c4aca8909966a2c97a95bf1232b4c0a7a6b39e
lukasz.rodak <lukasz.rodak@softiq.pl>
2017-06-22 22:03:55 +0200

    add new data article

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ $app->get('api/v1/article','ArticleController@index');
 $app->get('api/v1/article/{id}','ArticleController@getArticle');
 $app->post('api/v1/article','ArticleController@createArticle');
 $app->put('api/v1/article/{id}','ArticleController@updateArticle');
-$app->delete('api/v1/article','ArticleController@deleteArticle');
+$app->delete('api/v1/article/{id}','ArticleController@deleteArticle');
 
 
 
