b9a8832367403b78972c7761663eb21b7af53ffc
lukasz.rodak <lukasz.rodak@softiq.pl>
2017-06-22 22:06:23 +0200

    delete old codes

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
-$app->get('/', function () use ($app) {
-    return $app->version();
-});
 $app->get('api/v1/article','ArticleController@index');
 $app->get('api/v1/article/{id}','ArticleController@getArticle');
 $app->post('api/v1/article','ArticleController@createArticle');
 $app->put('api/v1/article/{id}','ArticleController@updateArticle');
 $app->delete('api/v1/article/{id}','ArticleController@deleteArticle');
 
-
-
-$app->get('tutaj','MealController@index');
-
-$app->post('tutaj','MealController@createMeal');
-$app->get('/nic', function () 
-{
-    $user = Testmongodb::all();
-    print_r($user);
-});
